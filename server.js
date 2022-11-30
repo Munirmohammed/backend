45bb78228302d4c114b94dac7634681c0ece1297
Munir Mohammed <munirmohammed038@gmail.com>
2022-11-30 17:29:52 +0300

    edited server

--- server.js
+++ server.js
@@ -19,7 +19,12 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(cors())
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'https://muninvent-app.vercel.app'],
+    credentials: true,
+  })
+)
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
